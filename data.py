d6402f0e96d2d351afcee2b3918f81ccb79c48aa
milene <milenesousa879@gmail.com>
2026-09-11 13:18:51 +0100

    "Correção dos arquivos para reenvio"

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 
-URBAN_ROUTES_URL = ''
+URBAN_ROUTES_URL = 'https://cnt-772056de-b298-4b63-acbf-7aa5ba6823bb.containerhub.tripleten-services.com/'
 
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
